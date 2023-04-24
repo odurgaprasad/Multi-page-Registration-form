f16d82ee6a13a1bd2d12649e478c9e0cd186c7b3
odurgaprasad <118603147+odurgaprasad@users.noreply.github.com>
2023-04-24 23:02:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Multi-page-Registration-form
+
+## Approach: 
+
+## This is a multi-page registration form that collects personal and educational information from the user. The form consists of three pages, where the first page contains personal information such as name, date of birth, email, parents' names, gender, nationality, home address, and telephone numbers. The second page contains educational information such as the name of the HSC institution, HSC group, HSC board, HSC roll, and passing year.The third pages contains upload documents such as the photo,hsc markssheet,ssc markssheet,all semister markssheet
+
+## The form is built using HTML,CSS,BOOTSTRAP AND JAVASCRIPT and it contains several input fields such as text, radio buttons, date picker, select, and pattern attributes to validate the user input. The form also has a Next button that allows the user to move from the first page to the second page.
+
+## To submit the form, the user needs to fill in all the required fields indicated by a star (*), and the telephone numbers should have ten digits. After submitting the form, the user's data can be stored in a database for further use.
+
+## When the user fills out the form, the data is stored in localStorage, and the form is hidden. Instead, a preview screen is displayed showing the user the information they entered.
+
+## The form is divided into three sections, and the user can navigate between them using "Next" and "Previous" buttons. The code also includes form validation to ensure that all required fields are filled in before the user can proceed.
+
+## Finally, there are "Reset" and "Preview" buttons on the preview screen. The Reset button allows the user to start over, and the Preview button takes the user to a new page where they can see a summary of their data before submitting the form.
+
+## The HTML code for this multi-page form is not shown here, but it should include three form sections (with IDs "form-page1", "form-page2", and "form-page3"), the "multi-page-form" form ID, and other relevant elements such as input fields, labels, and buttons. The JavaScript code uses these IDs to access and manipulate the form and its elements.
+
+## Technologies used: 
+ HTML, CSS, Javascript, BootStrap, LocalStorage
+
+
+ WEBPAGE LINK:http://127.0.0.1:5500/index.html
+
+ OR 
+
+ WEBPAGE liNK:https://nettantra.ccbp.tech/
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 
 
-window.addEventListener("load",()=>{
-  const form=document.getElementById("multi-page-form")
-  const first_form=document.getElementById("form-page1")
-  const second_form=document.getElementById("form-page2")
-  const third_form=document.getElementById("form-page3")
-  const first_next_button=document.getElementById("Next-Button-1")
-  const second_next_button=document.getElementById("Next-Button-2")
-  const third_next_button=document.getElementById("Next-Button-3")
-  const prev_button1=document.getElementById("Prev-Button-1")
-  const prev_button2=document.getElementById("Prev-Button-2")
-  const formContainerBox=document.getElementById("form-container-box")
-  const successBox=document.getElementById("success-Box")
-  const ResetBtn=document.getElementById("reset-btn")
-  const PreviewBtn=document.getElementById("preview-btn")
+window.addEventListener("load", () => {
+  const form = document.getElementById("multi-page-form")
+  const first_form = document.getElementById("form-page1")
+  const second_form = document.getElementById("form-page2")
+  const third_form = document.getElementById("form-page3")
+  const first_next_button = document.getElementById("Next-Button-1")
+  const second_next_button = document.getElementById("Next-Button-2")
+  const third_next_button = document.getElementById("Next-Button-3")
+  const prev_button1 = document.getElementById("Prev-Button-1")
+  const prev_button2 = document.getElementById("Prev-Button-2")
+  const formContainerBox = document.getElementById("form-container-box")
+  const successBox = document.getElementById("success-Box")
+  const ResetBtn = document.getElementById("reset-btn")
+  const PreviewBtn = document.getElementById("preview-btn")
 
-  
+
   // Handle form submission
-  form.addEventListener("submit",(e)=>{
+  // form.addEventListener("submit",(e)=>{
+  third_next_button.addEventListener("click", (e) => {
     e.preventDefault();
+
     const firstName = document.getElementById("first_name").value.trim();
     const lastName = document.getElementById("last_name").value.trim();
     const dob = document.getElementById("date").value.trim();
@@ -31,16 +33,16 @@ window.addEventListener("load",()=>{
     const nationality = document.getElementById("Nationality").value.trim();
     const homeAddress = document.getElementById("Home").value.trim();
     const city = document.getElementById("City").value.trim();
-    const country = document.getElementById("country").value;
+    const country = document.getElementById("country").value.trim();
     const telephoneHome = document.getElementById("Telephone-Home").value.trim();
     const mobile = document.getElementById("Mobile").value.trim();
     const hscInstitution = document.getElementById('hscInstitution').value.trim();
-    const hscBoard = document.getElementById('hscBoard').value;
+    const hscBoard = document.getElementById('hscBoard').value.trim();
     const hscScore = parseInt(document.getElementById('hscScore').value.trim());
     const sscInstitution = document.getElementById('sscInstitution').value.trim();
-    const sscBoard = document.getElementById('sscBoard').value;
+    const sscBoard = document.getElementById('sscBoard').value.trim();
     const sscScore = parseInt(document.getElementById('sscScore').value.trim());
-    const currentlyPursuing = document.getElementById('currently-pursuing').value;
+    const currentlyPursuing = document.getElementById('currently-pursuing').value.trim();
     const currentInstitution = document.getElementById('currentInstitution').value.trim();
     const overallScore = parseInt(document.getElementById('overallScore').value.trim());
     const currentBacklogs = parseInt(document.getElementById('currentBacklogs').value.trim());
@@ -48,14 +50,27 @@ window.addEventListener("load",()=>{
     const hscMarksheet = document.getElementById("hscMarksheet");
     const sscMarksheet = document.getElementById("sscMarksheet");
     const semesterMarksheet = document.getElementById("semesterMarksheet");
-    
-    if (firstName==="" || lastName==="" || dob==="" || email==="" || fatherFirstName==="" || fatherLastName==="" || motherFirstName==="" || motherLastName===""|| gender===""
-    || nationality===""|| homeAddress===""|| city===""|| country===""|| telephoneHome===""|| mobile===""|| hscInstitution===""
-    || hscBoard===""|| hscScore===""|| sscInstitution===""|| sscBoard===""|| sscScore===""|| currentlyPursuing===""|| currentInstitution===""
-    || overallScore===""|| currentBacklogs===""|| photo===""|| hscMarksheet===""|| sscMarksheet===""|| semesterMarksheet===""){
-      alert("please fill the form")
+
+    if (firstName === "" || lastName === "" || dob === "" || email === "" || fatherFirstName === "" || fatherLastName === "" || motherFirstName === "" || motherLastName === "" || gender === ""
+      || nationality === "" || homeAddress === "" || city === "" || country === "" || telephoneHome === "" ||telephoneHome.length === 10 || mobile.length === 10 || mobile === "" ) {
+      alert("Please fill the form completely before proceeding !!");
+      return false;
     }
-    
+     if ( hscInstitution === "" || hscBoard === "" || hscScore === "" || sscInstitution === "" || sscBoard === "" || sscScore === "" || currentlyPursuing === "" || currentInstitution === ""
+     || overallScore === "" || currentBacklogs === ""){
+      alert("Please fill the form completely before proceeding !!");
+      return false;
+     }
+     if (photo === "" || hscMarksheet === "" || sscMarksheet === "" || semesterMarksheet === ""){
+      alert("Please fill the form completely before proceeding !!");
+      return false;
+     }
+
+    // Hiding the form & displaying Preview Container
+    formContainerBox.style.display = "none";
+    successBox.style.display = "flex";
+
+    // list of data
     const formdata = {
       firstName,
       lastName,
@@ -87,55 +102,51 @@ window.addEventListener("load",()=>{
       sscMarksheet: sscMarksheet.files[0],
       semesterMarksheet: semesterMarksheet.files[0]
     };
-    console.log(localStorage.setItem("formData", JSON.stringify(formdata)));
-    formContainerBox.style.display="none";
+    // store the data to localStorage
+    console.log("formData", formdata);
+    localStorage.setItem("formData", JSON.stringify(formdata));
+    formContainerBox.style.display = "none";
   });
 
-  
-  
-  
-
-  first_next_button.addEventListener("click",()=>{
-    first_form.style.display="none";
-    second_form.style.display="block";
+// click on first_button hiding first_form &displaing second_form
+  first_next_button.addEventListener("click", () => {
+    first_form.style.display = "none";
+    second_form.style.display = "block";
   })
+  // click on first_button hiding second_form &displaing third_form
+  second_next_button.addEventListener("click", () => {
+    second_form.style.display = "none";
+    third_form.style.display = "block";
 
-  second_next_button.addEventListener("click",()=>{
-    second_form.style.display="none";
-    third_form.style.display="block";
-    
-  })
-
-
-  third_next_button.addEventListener("click",()=>{
-    formContainerBox.style.display="none";
-    successBox.style.display="flex"
-    
   });
-  
-
-  prev_button1.addEventListener("click",()=>{
-    first_form.style.display="block";
-    second_form.style.display="none"
+  // click on prev_button hiding second_form &displaing first_form
+  prev_button1.addEventListener("click", () => {
+    first_form.style.display = "block";
+    second_form.style.display = "none"
+  })
+  // click on prev_button hiding third_form &displaing second_form
+  prev_button2.addEventListener("click", () => {
+    third_form.style.display = "none";
+    second_form.style.display = "block"
   })
 
-  prev_button2.addEventListener("click",()=>{
-    third_form.style.display="none";
-    second_form.style.display="block"
-  })
-
-
-  ResetBtn.addEventListener("click",()=>{
-    successBox.style.display="none"
-    formContainerBox.style.display="none"
+// Reload the page
+  ResetBtn.addEventListener("click", () => {
+    successBox.style.display = "none"
+    formContainerBox.style.display = "none"
     window.location.reload()
   });
-
+// preview the data
   PreviewBtn.addEventListener("click", () => {
+    successBox.style.display = "none";
+
     const data = JSON.parse(localStorage.getItem("formData"));
-    const successBox = document.createElement("div");
-    successBox.classList.add("success-box");
-    successBox.innerHTML = `
+
+    const previewBox = document.createElement("div");
+    previewBox.classList.add("success-box");
+    previewBox.innerHTML = `
+      <p>Displaying data fetched from Local Storage</p>
+      <p>Student Details:</p>
       <p>Name: ${data.firstName} ${data.lastName}</p>
       <p>Date of birth: ${data.dob}</p>
       <p>Email: ${data.email}</p>
@@ -160,9 +171,9 @@ window.addEventListener("load",()=>{
       <p>Current Backlogs: ${data.currentBacklogs}</p>
     `;
     // append the successBox to the body of the HTML document
-    document.body.appendChild(successBox);
+    document.body.appendChild(previewBox);
   });
-  
+
 })
 
 
